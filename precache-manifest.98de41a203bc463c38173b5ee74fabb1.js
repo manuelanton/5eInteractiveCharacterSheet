514721a59b8810b8e0b9ff45e785618db79772e1
manuel-phinx <60482605+manuel-phinx@users.noreply.github.com>
2020-02-07 14:30:35 -0300

    Updates

---
+++ precache-manifest.98de41a203bc463c38173b5ee74fabb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852149f4374d659929e06f3869768183",
+    "url": "/5eInteractiveCharacterSheet/index.html"
+  },
+  {
+    "revision": "08dd5f2b688b8abd28bf",
+    "url": "/5eInteractiveCharacterSheet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f684d54ae37af8ddfebd",
+    "url": "/5eInteractiveCharacterSheet/static/js/2.af814d0e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/5eInteractiveCharacterSheet/static/js/2.af814d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08dd5f2b688b8abd28bf",
+    "url": "/5eInteractiveCharacterSheet/static/js/main.d1e7b36d.chunk.js"
+  },
+  {
+    "revision": "03de8aecdc91ee8d4072",
+    "url": "/5eInteractiveCharacterSheet/static/js/runtime-main.79975e3f.js"
+  }
+]);
