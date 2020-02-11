a4ccf02a26c984de53c1d0cd177104c48fea1a37
manuel-phinx <60482605+manuel-phinx@users.noreply.github.com>
2020-02-11 14:18:01 -0300

    saving throw functionality added

--- src/components/SingleStat.tsx
+++ src/components/SingleStat.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { StatCard, H5 } from "../components/styles/StatCard";
 import { createStatChange, addSave, removeSave } from "../actions/stats";
+import { diceRoller } from "../utils";
 
 const SingleStat = (props: { name: string; stat: string }) => {
   const prof: any = useSelector<any>(state => state.saves);
   const statValue: any = useSelector<any>(state => state.stats[props.stat]);
+  const profBonus: any = useSelector<any>(state => state.profBonus);
   const dispatch = useDispatch();
 
   const addOrRemoveSave = () => {
@@ -14,6 +16,10 @@ const SingleStat = (props: { name: string; stat: string }) => {
       : addSave(props.stat);
   };
 
+  const saveBonus: number = prof.includes(props.stat)
+    ? Math.floor((statValue - 10) / 2) + profBonus
+    : Math.floor((statValue - 10) / 2);
+
   return (
     <StatCard>
       <input
@@ -21,7 +27,9 @@ const SingleStat = (props: { name: string; stat: string }) => {
         checked={prof.includes(props.stat)}
         onChange={() => dispatch(addOrRemoveSave())}
       />
-      <H5>{props.name.toUpperCase()}</H5>
+      <H5 onClick={() => alert(`Total: ${diceRoller(20, 1, saveBonus).total}`)}>
+        {props.name.toUpperCase()}
+      </H5>
       <span>
         {props.stat}: {Math.floor((statValue - 10) / 2)}
       </span>
